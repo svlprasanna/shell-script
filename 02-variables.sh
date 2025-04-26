c70091969b7dad2a96ec936fc910f0475ff2f773
svlprasanna <svlprasanna92@gmail.com>
2025-04-27 00:14:43 +0530

    variables script added

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh: hello suresh, good morning"
+echo "suresh: hey ramesh, its been so long, very good morning"
+echo "ramesh: yeah, how are you ? "
+echo "suresh: yup, am doing good, how about you ? "
