2a29dfe6dc2c3a1336c17c09963222b0b1b3eeea
svlprasanna <svlprasanna92@gmail.com>
2025-05-01 16:54:53 +0530

    datatypes and arrays concept

---
+++ 06-datatypes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Number1=$1
+Number2=$2
+
+Sum=$(($Number1+$Number2))
+echo "Sum is $Sum"
---
+++ 07-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Fruits=("apple" "banana" "mango")
+
+echo "first fruit is ${Fruits[0]}"
+echo "second fruit is ${Fruits[1]}"
+echo "third fruit is ${Fruits[2]}"
