545ac1b9e455c28109a68a8dd2d461acaeaea342
svlprasanna <svlprasanna92@gmail.com>
2025-05-01 23:01:55 +0530

    functions script

---
+++ 10-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+    echo "ERROR : installing mysql is failed"
+    exit 1
+    else
+    echo "installing mysql is success..."
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+echo "error:please run with root access"
+exit 1
+else
+echo "you are root user"
+fi
+
+yum install mysql -y
+VALIDATE
+
+yum install git -y
+VALIDATE
