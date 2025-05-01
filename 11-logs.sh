e561dcc702270f33cea471db473a91b2f92002c2
svlprasanna <svlprasanna92@gmail.com>
2025-05-01 23:55:50 +0530

    how to store logs

---
+++ 11-logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name is : $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo "ERROR : installing $2 is failed"
+    exit 1
+    else
+    echo "installing $2 is success..."
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+echo "error:please run with root access"
+exit 1
+else
+echo "you are root user"
+fi
+
+yum install mysql -y
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+VALIDATE $? "installing git"
