c3d1077c2eba08490008be9a144f838c8e27db7d
svlprasanna <svlprasanna92@gmail.com>
2025-05-02 00:15:41 +0530

    how to store logs

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script name is : $0"
 
@@ -22,8 +24,8 @@ else
 echo "you are root user"
 fi
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 VALIDATE $? "Installing mysql"
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 VALIDATE $? "installing git"
