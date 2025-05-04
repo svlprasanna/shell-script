620257e9fe181f936fa69d24a5c187e1d58e8617
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 00:04:55 +0530

    delete old logs

---
+++ 15-delete_oldlogs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SOURCE_FILE=/tmp/shellscript_logs
+
+if [ ! -d $SOURCE_FILE ]
+then
+echo -e "source file doesn't exist"
+else
+echo -e "source file exists"
+fi
+
+files_to_delete=$(find . -type f -mtime +14 -name "*.log)
+while IFS= read -r line
+do
+    echo "deleting old log files $line
+done
