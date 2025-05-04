980f8b64f7089ea40243acf461c6b72adc0e04e9
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 00:30:57 +0530

    while loop

--- ifs.sh
+++ ifs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SOURCE_FILE="/etc/passwd"
+SOURCE_FILE=/etc/passwd
 
 if [ ! -d $SOURCE_FILE ]
 then
 echo -e "source file doesn't exist"
+exit 1
 else
 echo -e "source file exists"
 fi
