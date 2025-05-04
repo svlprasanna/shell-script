bfe4b2b88d3586c2cd44a6dbc99571124cf0c3d6
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 02:31:39 +0530

    word-count

--- count-words.sh
+++ count-words.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=/home/centos/shell-script/text.txt
+FILE="/home/centos/shell-script/text.txt"
 
 if [ ! -f $FILE ]
 then    
@@ -9,5 +9,8 @@ else
 echo "file exist in the system"
 fi
 
-word=$(echo -n "$FILE" | wc -w)
-echo "words in the file are $word"
+while IFS= read -s $FILE
+do
+count=$(cat $FILE | wc -w " ")
+echo "words in the file are: $count)
+done
