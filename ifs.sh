f4226275c3137420ae18cbf65cb041e1d14072b1
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 00:28:10 +0530

    while loop

---
+++ ifs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE_FILE="/etc/passwd"
+
+if [ ! -d $SOURCE_FILE ]
+then
+echo -e "source file doesn't exist"
+else
+echo -e "source file exists"
+fi
+
+while IFS=":" read -r username password user_id group_id user_fullname home_dir
+do
+echo "username: $username"
+echo "user ID: $user_id"
+echo "password: $password"
+done
