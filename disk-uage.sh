8a54306cd9c675fab0e4f04c043172a3f56794fc
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 14:35:38 +0530

    disk usage

--- disk-uage.sh
+++ disk-uage.sh
@@ -3,7 +3,8 @@
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 THRESHOLD=1
 
-while IFS= read -r line
+while IFS= read line
+do
 usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
 partition=$(echo $lline | awk '{print $1F}')
 
