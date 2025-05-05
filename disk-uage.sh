b37dd56841fcbda782d28397c56b04aa138ecf27
svlprasanna <svlprasanna92@gmail.com>
2025-05-05 14:32:56 +0530

    disk usage

---
+++ disk-uage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+THRESHOLD=1
+
+while IFS= read -r line
+usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+partition=$(echo $lline | awk '{print $1F}')
+
+if [ $usage -gt $partition ]
+then
+    message+="disk usage on $partition is: $usage"
+    fi
+done <<< $DISK_USAGE
+
+echo "MESSAGE:$message"
+
+echo "$MESSAGE" | mail -s "high disk usage" lakshmimohan1216@gmail.com
+
+
